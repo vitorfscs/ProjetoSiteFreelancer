6bb10cceb09cfd75e8df4e3d2add06e9556c9d23
João Vitor Fonseca da Silva <144865696+vitorfscs@users.noreply.github.com>
2024-06-09 18:54:26 -0300

    Add files via upload

---
+++ module-info.java
@@ -0,0 +1,15 @@
+package com.exemplo.whatsappchat;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+
+public class WhatsappController {
+	
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}
+}
